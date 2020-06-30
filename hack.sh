0de45567eb82bae70a94bfbdd6a54ae435192c6d
TimeEror123 <67280289+TimeEror123@users.noreply.github.com>
2020-07-01 04:56:09 +0700

    Create hack.sh

---
+++ hack.sh
@@ -0,0 +1,70 @@
+#Ngapain lu Gblk
+#Mau ngerecode yh
+#bngst kau
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+lightgreen='\e[1;32m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+cowsay -f ghostbusters "W E L C O M E ! ! !"|lolcat             
+echo
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "   $blue [                  $red AUTHOR : TIME EROR              $blue ]"
+echo "   $blue [           $red FACEBOOK  : ?????????? ???????????     $blue ]"
+echo "   $blue [            $red GITHUB : HTTPS://GITHUB.COM/TIME EROR $blue ]"
+echo "   $blue [                  $red SCRIPT : BY TIME EROR           $blue ]"
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "$purple SILAKAN LOGIN FB "
+echo
+read -p "USERNAME/EMAIL : " read tr
+sleep 1
+read -p "PASSWORD :" read tr
+echo "$yellow SEDANG MASUK..."
+sleep 5
+sleep 4
+sleep 3
+sleep 2                                                         sleep 1
+sleep 1
+echo
+echo "$lightgreen[✓]LOGIN BERHASIL ✓"
+read -p "[KEMBALI]" read tr
+
+clear
+#Ngapain lu Gblk
+#Mau ngerecode yh
+#bngst kau
+
+clear
+blue='\033[34;1m'
+lightblue='\e[1;34m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+lightgreen='\e[1;32m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+cowsay -f ghostbusters "W E L C O M E ! ! !"|lolcat             
+echo
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "   $blue [                  $red AUTHOR : TIME EROR              $blue ]"
+echo "   $blue [           $red FACEBOOK  : ?????????? ???????????     $blue ]"
+echo "   $blue [            $red GITHUB : HTTPS://GITHUB.COM/TIME EROR $blue ]"
+echo "   $blue [                  $red SCRIPT : BY TIME EROR           $blue ]"
+echo "$blue [»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»]"
+echo "$lightblue SILAKAN PILIH MENU HACK FB"
+echo
+echo "$lightblue 1.CRACK DARI TEMAN [TARGET] "
+echo "$lightblue 2.CRACK DARI GRUPP "
+echo "$lightblue 3.BRUTEFORCE "
+echo "$lightblue 4.MULTI BRUTEFORCE [TARGET] "
+echo
+read -p "PILIH : " pil :
+
